cc179c6321ecc2868dab3e509943d747cc7f9b47
Florian FERBACH <florian@marmelab.com>
2016-08-31 13:52:35 +0200

    Adds view game controller & template

---
+++ src/AppBundle/Controller/GameController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ConnectFour\Board;
+
+class GameController extends Controller
+{
+    /**
+     * Views a game at its last step.
+     *
+     * @Route("/game/{gameId}", name="viewGame")
+     */
+    public function viewAction($gameId)
+    {
+        $gameManager = $this->get('app.game.manager');
+        $game = $gameManager->getGame($gameId);
+
+        // TODO : add session check for player nickname
+        $status = $game->getStatus();
+
+        return $this->render("game/$status.html.twig", array(
+          'game' => $game,
+          'columnBounds' => Board::COLUMNS,
+          'rowBounds' => Board::ROWS,
+      ));
+    }
+}
